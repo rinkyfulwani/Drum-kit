4c8942eebd9f1917402d86b138c4128727d7ae88
rinkyfulwani <79286720+rinkyfulwani@users.noreply.github.com>
2021-04-21 12:15:31 -0700

    Starting files for Drum kit

---
+++ index.js
@@ -0,0 +1,56 @@
+var buttons = document.getElementsByClassName("drum");
+
+for (var i = 0; i < buttons.length; i++) {
+  document.addEventListener("keydown", function(event) {
+    handleClick(event.key);
+    buttonAnimation(event.key);
+  });
+  buttons[i].addEventListener("click", function() {
+    handleClick(this.innerText);
+    buttonAnimation(this.innerText);
+  });
+}
+
+function handleClick(buttonType) {
+  console.log(buttonType);
+  switch (buttonType) {
+    case "w":
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "a":
+      var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+    default:
+
+  }
+}
+
+function buttonAnimation(key) {
+  var activeButton = document.querySelector("."+key);
+  activeButton.classList.add("pressed");
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 1000);
+}
